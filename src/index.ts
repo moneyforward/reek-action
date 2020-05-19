786eb152c3f08ce95c3cb6ed65c1323fd0580ccd
Naoki Kimura <n.kimura.cap@gmail.com>
2020-05-19 11:01:14 +0900

    Use Code review action

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,56 @@
-import Analyzer from './analyzer';
+import stream from 'stream';
+import util from 'util';
+import StaticCodeAnalyzer, { AnalyzerConstructorParameter, installer } from '@moneyforward/sca-action-core';
+import { transform } from '@moneyforward/stream-util';
 
-console.log('::echo::%s', process.env['RUNNER_DEBUG'] === '1' ? 'on' : 'off');
-(async (): Promise<void> => {
-  const files = process.env.INPUT_FILES || '.';
-  const options = JSON.parse(process.env.INPUT_OPTIONS || '[]');
-  const workingDirectory = process.env.INPUT_WORKING_DIRECTORY;
-  workingDirectory && process.chdir(workingDirectory);
-  const analyzer = new Analyzer(options);
-  analyzer.reporterTypeNotation = process.env.INPUT_REPORTER_TYPE_NOTATION;
-  process.exitCode = await analyzer.analyze(files);
-})().catch(reason => {
-  console.log(`::error::${String(reason)}`);
-  process.exit(1);
-});
+const debug = util.debuglog('@moneyforward/code-review-action-reek-plugin');
+
+interface Smell {
+  context: string;
+  lines: number[];
+  message: string;
+  smell_type: string;
+  source: string;
+  documentation_link: string;
+}
+
+export type Result = Smell[];
+
+export default class Analyzer extends StaticCodeAnalyzer {
+  private static readonly command = 'reek';
+
+  constructor(...args: AnalyzerConstructorParameter[]) {
+    super(Analyzer.command, args.map(String).concat(['-f', 'json']), undefined, exitStatus => exitStatus === 0 || exitStatus === 2, undefined, 'Reek');
+  }
+
+  protected async prepare(): Promise<void> {
+    console.log(`::group::Installing gems...`);
+    try {
+      await new installer.RubyGemsInstaller(true).execute([Analyzer.command]);
+    } finally {
+      console.log(`::endgroup::`)
+    }
+  }
+
+  protected createTransformStreams(): stream.Transform[] {
+    return [
+      new transform.JSON(),
+      new stream.Transform({
+        objectMode: true,
+        transform: function (result: Result, encoding, done): void {
+          debug(`Detected %d problem(s).`, result.length);
+          for (const smell of result) for (const line of smell.lines) this.push({
+            file: smell.source,
+            line: line,
+            column: undefined,
+            severity: 'warning',
+            message: smell.message,
+            code: smell.smell_type
+          });
+          this.push(null);
+          done();
+        }
+      })
+    ];
+  }
+}
